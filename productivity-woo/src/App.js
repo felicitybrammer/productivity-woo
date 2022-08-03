cdb7e7da18eed13e3f8f46d0fd2d8b8036bdf9a9
Felicity Brammer <felicitybrammer@gmail.com>
2022-08-02 22:37:08 -0700

    single component for mapping

--- productivity-woo/src/App.js
+++ productivity-woo/src/App.js
@@ -2,14 +2,16 @@ import todos from '/mock-todos.json';
 import './App.css';
 import Title from './Title';
 import React, { useState } from 'react';
+import ToDoList from './ToDoList';
 
 function App() {
   
-  const [toDo, setToDo] = useState(todos);
+  const [toDoList, setToDo] = useState(todos);
   
   return (
     <div className="App">
       <Title />
+      <ToDoList toDoList={todos} />
     </div>
   );
 }
